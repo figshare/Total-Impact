438021f4fb1fec945a1bf6778071c16aecc44f2a
Cristhian Daniel Parra Trepowski <cdparra@gmail.com>
2011-05-11 13:30:55 +0100

    added mendely class

--- plugins/mendeley/Mendeley.php
+++ plugins/mendeley/Mendeley.php
@@ -5,8 +5,38 @@
  */
 
 class Mendeley {
-  
+    public $id;
+    public $method;
+    public $metric_name;
+    public $metric_value;
+    public $source_name;
+    public $icon;
+    public $type;
 
-  
+    //protected $map = array();
+    //protected $errorClasses = array();
+    //protected $cached;
 
+    /**
+     * The constructor.
+     *
+     * @param string $mode The mode, either debug or production
+     */
+    public function  __construct($id = 'null')
+    {
+        $this->id = $id;
+    }
+    
+    public getMetrics() {
+        return this->getTestMetrics();
+    }
+    
+    public getTestMetrics() {
+        this->method = 'GET';
+        this->metric_name = 'Readership';
+        this->metric_value = 50;
+        this->source_name = 'Mendeley';
+        this->icon = 'http://www.mendeley.com/favicon.ico';
+        this->type = 'Article';
+    }
 }
