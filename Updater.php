e0b418126506052855533cacc35b70b4f2eb0039
Jason Priem <jay@Jason-Priems-MacBook-Air.local>
2011-05-11 22:47:10 +0100

    reporting changes

--- Updater.php
+++ Updater.php
@@ -21,18 +21,26 @@ class Updater {
             ->include_docs(true)
             ->getView("main", "unupdated");
         
-        foreach ($result->rows as $row){
-            $doc = $row->doc;
-            foreach ($this->config['plugins'] as $sourceName=>$sourceUri){
-                $this->http->setUri($sourceUri);
-                print_r($doc->artifact_ids);
-                
-                print_r(json_encode($doc->artifact_ids));
-                echo "<br><br>";
-                $this->http->setRawData(json_encode($doc->artifact_ids), 'text/json');  
-                $result = $this->http->request("POST");
-                $doc->sources->{$sourceName} = $result->getBody();
-                
+        if (count($result->rows) == 0){
+            echo "Everything's up to date.";
+        }
+        else { // there's stuff that hasn't been updated.
+        
+            foreach ($result->rows as $row){
+                $doc = $row->doc;
+                foreach ($this->config['plugins'] as $sourceName=>$sourceUri){
+                    $this->http->setUri($sourceUri);
+                    print_r($doc->artifact_ids);
+
+                    $this->http->setRawData(json_encode($doc->artifact_ids), 'text/json');  
+                    $result = $this->http->request("POST");
+                    $doc->sources->{$sourceName} = $result->getBody();
+                    $doc->updated = true;
+                    print_r($doc);
+                    echo "<br><br>";
+                    $this->couch->storeDoc($doc);
+
+                }
             }
         }
     }
