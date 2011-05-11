f58fcb042df0a78b7a4b33dec91e2b2b8cd912c5
Cristhian Daniel Parra Trepowski <cdparra@gmail.com>
2011-05-12 00:17:15 +0100

    added plugin controller for slideshare

---
+++ plugins/slideshare/index.php
@@ -0,0 +1,16 @@
+<?php
+include '../common/RestServer.php';
+include '../common/Metric.php';
+include '../common/MetricList.php';
+include 'SlidesharePluginController.php';
+
+spl_autoload_register(); // don't load our classes unless we use them
+
+$mode = 'debug'; // 'debug' or 'production'
+$server = new RestServer($mode);
+// $server->refreshCache(); // uncomment momentarily to clear the cache if classes change in production mode
+
+$server->addClass('SlidesharePluginController');
+
+$server->handle();
+?>
