5385fcf751a0873095de3ec660c04ccc1420b593
Jason Priem <jason@jasonpriem.com>
2011-05-12 01:50:36 +0100

    upload and update work YAY

--- config/configs.php
+++ config/configs.php
@@ -2,7 +2,7 @@
 $configs = array(
     
     'plugins' => array(
-//        'Mendeley' => 'http://researchremix.org/total-impact/mendeley/metrics',
+        'Mendeley' => 'http://researchremix.org/total-impact/mendeley/metrics',
         'Slideshare' => 'http://researchremix.org/total-impact/slideshare/metrics',
         'Dryad' => 'http://researchremix.org/total-impact/dryad/metrics'
     ),
