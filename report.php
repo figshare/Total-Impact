bc23174d10d9e2cacb10756a2d3ac44e3fb02f72
Heather Piwowar <hpiwowar@gmail.com>
2011-05-12 01:31:54 +0100

    metric detail added by Aliaksandr Birukou

--- report.php
+++ report.php
@@ -22,7 +22,7 @@ $title = $doc->title;
 <title>Total Impact</title>
     <script type="text/javascript" src="./library/jquery/jquery-1.4.2.js"></script>
 	<script type="text/javascript" src="./library/jquery/jquery.tools.min.js"></script>
-	 <script type="text/javascript" src="./library/protovis-3.2/protovis-r3.2.js"></script>
+	<script type="text/javascript" src="./library/protovis-3.2/protovis-r3.2.js"></script>
 	
 	<script language="javascript" type="text/javascript">
         
@@ -30,7 +30,8 @@ $title = $doc->title;
         
         $(document).ready(function() {
 			console.log("Starting up");
-			/*
+			
+			/* 
 			source1 = {};
 			source1['type'] = "presentation";
 			source1['metric_value'] = 200;
@@ -57,7 +58,8 @@ $title = $doc->title;
        		artifacts['http://www.ncbi.nlm.nih.gov/pubmed/18466268'] = artifact2;
 			
 			*/
-                        artifacts = eval(<?php echo $sourcesData ?>);
+            
+			artifacts = eval(<?php echo $sourcesData ?>);
                         
 			artTotal = 0;
 			presTotal = 0;
@@ -232,6 +234,74 @@ Totals by type<br>
 
 <div id="posts" class="section">
 </div>
+
+<!-- 2011-05-12 ADDED by Aliaksandr Birukou-->
+<div id="legend" class="section">
+Metrics are computed based on the following data sources:
+
+<ul type="square" class="body">
+<li>
+<a href="http://www.crossref.org/"><img src="http://www.crossref.org/favicon.ico" border='0'>CrossRef</a> - an official Digital Object Identifier (DOI) Registration Agency of the International DOI Foundation. For a <strong>DOI</strong> it returns: 
+<ul>
+	<li><strong><span class="param">journal</span></strong>: <span class="desc">the journal, where the paper is published,</span></li>
+	<li><strong><span class="param">pubdate</span></strong>: <span class="desc">the year of the publication,</span></li> 
+	<li><strong><span class="param">title</span></strong>: <span class="desc">the title of the publication.</span></li>
+</ul></li>
+</ul>
+<ul type="square" class="body">
+<li>
+<a href="http://datadryad.org/"><img src="dryadfavicon.ico" border='0'>Dryad</a> - an international repository of data underlying peer-reviewed articles in the basic and applied biosciences. For a <strong>DOI</strong> it returns:
+<ul>
+	<li><strong><span class="param">pageviews</span></strong>: <span class="desc">the number of views of the item by Dryad users.</span></li>
+</ul></li>
+</ul>
+<ul type="square" class="body">
+<li>
+<a href="http://www.facebook.com/"><img src="http://www.facebook.com/favicon.ico" border='0'>Facebook</a> - a social networking service. For a <strong>DOI</strong> it returns: 
+   <ul>
+   	<li><strong><span class="param">likes</span></strong>: <span class="desc">the number of users who liked the article,</span></li>
+   	<li><strong><span class="param">shares</span></strong>: <span class="desc">the number of users who shared the article,</span></li>
+   	<li><strong><span class="param">comments</span></strong>: <span class="desc">the number of users who commented on the article,</span></li>
+   	<li><strong><span class="param">clicks</span></strong>: <span class="desc">the number of users who clicked on the article.</span></li>
+</ul></li>
+</ul>
+<ul type="square" class="body">
+<li>
+<a href="http://www.icpsr.umich.edu/icpsrweb/ICPSR/"><img src="http://www.icpsr.umich.edu/favicon.ico" border='0'>Inter-University Consortium for Political and Social Research (ICPSR)</a> - an organization that provides access to an extensive collection of downloadable data.
+ For a <strong>DOI</strong> it returns: 
+<ul>
+	<li><strong><span class="param">related_refs</span></strong>: <span class="desc">number of related articles, as collected by ICPSR.</span></li>
+</ul></li>
+</ul>
+<ul type="square" class="body">
+<li>
+<a href="http://www.mendeley.com/"><img src="http://www.mendeley.com/favicon.ico" border='0'>Mendeley</a> - a research management tool for desktop and web.
+ For a <strong>DOI</strong> it returns:
+<ul>
+	<li><strong><span class="param">readers</span></strong>: <span class="desc">the number of readers of the article,</span></li>
+	<!--<li><strong><span class="param">title</span></strong>: <span class="desc">the title of the article,</span></li>
+	<li><strong><span class="param">publication_outlet</span></strong>: <span class="desc">the place where the article was published,</span></li>
+	<li><strong><span class="param">year</span></strong>: <span class="desc">the year of the article on Mendeley,</span></li>
+	<li><strong><span class="param">authors</span></strong>: <span class="desc">the authors of the article.</span></li>-->
+</ul></li>
+</ul>
+<ul type="square" class="body">
+<li>
+<a href="http://www.slideshare.net/"><img src="http://www.slideshare.net/favicon.ico" border='0'>Slideshare</a> - the best way to share presentations, documents and professional videos. 
+<ul>
+	<li><strong><span class="param">downloads</span></strong>: <span class="desc">the number of downloads of the presentation,</span></li>
+	<li><strong><span class="param">favorites</span></strong>: <span class="desc">the number of users who added the presentation in their favorites,</span></li>
+	<li><strong><span class="param">comments</span></strong>: <span class="desc">the number of comments on the presentation,</span></li>
+	<li><strong><span class="param">views</span></strong>: <span class="desc">the number of views of the presentation.</span></li>
+</ul></li>
+</ul>
+
+In this initial release the data is only fetched the first time you use the service. In the future we are planning to refresh the data once a day.
+
+
+</div>
+<!-- END OF ADDED by Aliaksandr Birukou-->
+
 
 </div>
 
