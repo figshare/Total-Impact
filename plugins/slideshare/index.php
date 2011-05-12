8e4c8a16dc288851c8a455f9a3e1edfc22d706d8
root <root@vic.whohastodoit.com>
2011-05-12 05:23:45 +0000

    changed path of plugins

--- plugins/slideshare/index.php
+++ plugins/slideshare/index.php
@@ -9,8 +9,6 @@ spl_autoload_register(); // don't load our classes unless we use them
 $mode = 'debug'; // 'debug' or 'production'
 $server = new RestServer($mode);
 // $server->refreshCache(); // uncomment momentarily to clear the cache if classes change in production mode
-
 $server->addClass('SlidesharePluginController');
-
 $server->handle();
 ?>
