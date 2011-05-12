c5ff5c6d0cbee82945f373b25d63232ba88d6f2b
Jason Priem <jason@jasonpriem.com>
2011-05-12 05:48:11 +0100

    it's late.

--- report.php
+++ report.php
@@ -8,7 +8,9 @@ function display($id, Couch_Client $couch) {
 }
 
 $artifactId = $_GET['id'];
-$doc = display($artifactId, $couch);
+//$doc = display($artifactId, $couch);
+
+phpinfo();
 
 
 $sourcesData = json_encode($doc->sources);
