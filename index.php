c343a6741ac2e6b50dd3f868e502cfdb8768cea4
Heather Piwowar <hpiwowar@gmail.com>
2011-05-16 12:00:13 -0400

    add google analytics code

--- index.php
+++ index.php
@@ -4,6 +4,23 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>total-impact.org</title>
         <link rel="stylesheet" type="text/css" href="./ui/totalimpact.css" />
+
+
+		<script type="text/javascript">
+		//Google Analytics code
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-23384030-1']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+
+		</script>
+
     </head>
     <body>
         <div id="header">
