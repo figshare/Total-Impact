2a5581db966948ef84fa6f784317a1744dbcd278
Heather Piwowar <hpiwowar@gmail.com>
2011-05-18 09:09:05 -0400

    disable crossref plugin for now

--- config/configs.php
+++ config/configs.php
@@ -5,7 +5,6 @@ $configs = array(
         'Mendeley' => 'http://total-impact.org/plugins/mendeley/metrics',
         'Plosalm' => 'http://total-impact.org/plugins/plosalm/metrics',
         'Slideshare' => 'http://total-impact.org/plugins/slideshare/metrics',
-        'CrossRef' => 'http://total-impact.org/plugins/crossref/metrics',
         'Facebook' => 'http://total-impact.org/plugins/facebook/metrics',
         'ICPSR' => 'http://total-impact.org/plugins/icpsr/metrics',
         'Dryad' => 'http://total-impact.org/plugins/dryad/metrics'
