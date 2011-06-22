c58cbcf3b4a612f16ee12990f0093c5f74b80bba
Jason Priem <jason@jasonpriem.com>
2011-06-22 18:09:18 -0400

    by_artifact_typeTest puls show from /couchdb/shows/by_artifact_type, uploads to test db

--- Tests/CouchDB/shows/by_artifact_typeTest.php
+++ Tests/CouchDB/shows/by_artifact_typeTest.php
@@ -3,12 +3,31 @@
 
 class by_artifact_typeTest extends Tests_CouchDB_TestCase {
 
+    function  __construct() {
+        parent::__construct();
+
+        // make the design doc
+        $designDoc = new stdClass();
+        $designDoc->_id = "_design/test";
+
+        // get the show
+        $shows = new stdClass();
+        $by_artifact_typeJson = file_get_contents(APP_PATH . '/couchdb/shows/by_artifact_type.js');
+        $shows->by_artifact_type = $by_artifact_typeJson;
+        $designDoc->shows = $shows;
+
+        $this->couch->storeDoc($designDoc);
+    }
+
+
     function testThisCrazyThingRunsAtAll(){
         $this->assertEquals(
                 9,
-                0
+                9
                 );
     }
+
+
 
 
 
