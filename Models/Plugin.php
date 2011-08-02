2271be70fb4fd17ab0fea30031c29457b1ad8728
Heather Piwowar <hpiwowar@gmail.com>
2011-08-02 16:30:30 -0700

    change to GET request to plugins for now to help with debugging
    TODO:  later change this to a POST with a param key.  Will also have to
    change the plugin interfaces to use $_REQUEST

--- Models/Plugin.php
+++ Models/Plugin.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * A thin wrapper around Zend_Http_Client to handle communication with a given plugin
  *
@@ -14,6 +15,7 @@ class Models_Plugin extends Zend_Http_Client {
     public function setName($name) {
         $this->name = $name;
     }
+
     public function setArtifactIds(stdClass $artifactIds) {
         $this->artifactIds = $artifactIds;
     }
@@ -21,12 +23,11 @@ class Models_Plugin extends Zend_Http_Client {
         if (!isset($this->artifactIds)){
             throw new Exception("There are no artifact IDs loaded to send the Source API.");
         }
-        parent::setRawData(json_encode($this->artifactIds), 'text/json');
-        return parent::request("POST");
-
+		
+        #Change request to GET with a parameter key for now to help with debugging.  Later change to a POST with param key.
+        parent::setParameterGet("query", json_encode($this->artifactIds));
+        return parent::request("GET");
     }
-
-
 
 
 }
