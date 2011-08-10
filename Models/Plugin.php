9d2af09cc93a0e7a2d1ce9fdd7c8b11b77dfab4e
Heather Piwowar <hpiwowar@gmail.com>
2011-08-10 10:00:49 -0700

    change plugin call from GET back to POST

--- Models/Plugin.php
+++ Models/Plugin.php
@@ -20,13 +20,18 @@ class Models_Plugin extends Zend_Http_Client {
         $this->artifactIds = $artifactIds;
     }
     public function fetchData(){
+		#FB::log("in fetchData");
         if (!isset($this->artifactIds)){
+			#FB::log("no artifactIds");
             throw new Exception("There are no artifact IDs loaded to send the Source API.");
         }
+
+		#FB::log($this);
 		
-        #Change request to GET with a parameter key for now to help with debugging.  Later change to a POST with param key.
-        parent::setParameterGet("query", json_encode($this->artifactIds));
-        return parent::request("GET");
+        parent::setParameterPost("query", json_encode($this->artifactIds));
+		#FB::log(json_encode($this->artifactIds));
+		#FB::log(json_encode(" after set timeout"));		
+        return parent::request("POST");
     }
 
 
