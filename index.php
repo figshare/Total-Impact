5e0cb0ac035c4aa6b08349a7f77350df598a14ad
dartar <dartar@nitens.org>
2011-09-28 23:54:16 -0700

    3-column layout for index.php, added tooltips, XHTML compliance, minor edits

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ ob_implicit_flush(TRUE);
         <title>Total Impact</title>
         <link rel="stylesheet" type="text/css" href="./ui/totalimpact.css" />
 
+		<script type="text/javascript" src="./ui/js/tooltip.js"></script>
 		<script type="text/javascript">
 		//Google Analytics code
 		  var _gaq = _gaq || [];
@@ -100,102 +101,102 @@ $(document).ready(function(){
 			<!-- START instr -->
 
 	        <div id="instr">
-	            <p>Enter below the identifiers for a collection of artifacts you want to track. We'll provide you a permanent URL to automatically update statistics about this collection.  Peruse <a target="_blank" href="./report.php?id=hljHeI">a sample</a>, <a href="#quick">quick reports</a>, and <a href="#recent">recently-shared reports</a>.</p>
+	            <p class="howto">Enter below the identifiers for a collection of artifacts you want to track. We'll provide you a permanent URL to track statistics about this collection. You can peruse <a target="_blank" href="./report.php?id=hljHeI">a sample</a>, <a href="#quick">quick reports</a>, and <a href="#recent">recently-shared reports</a>.</p>
 	
+				<!--
 	            <p>To try it out, copy and paste these identifers below and hit <b>Go!</b> 
 	            <pre>
-	10.1371/journal.pbio.0050082
-	10.1371/journal.pone.0000308
-	http://www.slideshare.net/phylogenomics/eisenall-hands
-	10.5061/dryad.8384
-	GSE2109
-	</pre>
+				10.1371/journal.pbio.0050082
+				10.1371/journal.pone.0000308
+				http://www.slideshare.net/phylogenomics/eisenall-hands
+				10.5061/dryad.8384
+				GSE2109
+				</pre>
+				-->
 			</div>
 			<!-- END instr -->
 			
 			<!-- START input -->
 			<div id="input"> 	
-				<div id="leftcol">
+				<div id="col1">
 					<form name="id_form">
-			           <label for="name">Choose a name for this collection</label><br />
-			           <input name="name" id="name" value="<?php echo $title; ?>" />
-			           <br />
-			           
-			           <label for="list">List your IDs here:</label><br>
+					<fieldset><legend>Create a collection:</legend>
+			           <p><label for="list">List your IDs here</label><a class="tooltip" onmouseover="tooltip.show('Valid identifiers are: DOIs, Slideshare links, dataset IDs', 200);" onmouseout="tooltip.hide();"><sup>1</sup></a></p>
 			           <textarea rows=20 name="list" id="artifactList"><?php echo $artifactIdsString; ?></textarea>
+
+			           <p><label for="name">Name this collection</label><a class="tooltip" onmouseover="tooltip.show('You can add a custom name to identify this collection', 200);" onmouseout="tooltip.hide();"><sup>2</sup></a></p>
+			           <input name="name" id="name" value="<?php echo $title; ?>" />
+
 			           <input type="submit" name="run" value="Go!" />
+					</fieldset>
 					</form>
 							
 				</div>
-				<div id="rightcol">
-					Want help gathering your IDs? Pull from these sources:
-					<hr />
-					
-					<p>Mendeley profile <b>publicly available</b> publications:
-					<p><em>http://www.mendeley.com/profiles/</em>
+				<div id="col2">
+					<!--Want help gathering your IDs? Pull from these sources:-->
+					<fieldset><legend>Import from a Mendeley profile</legend>
+					<p>Your Mendeley profile URL<a class="tooltip" onmouseover="tooltip.show('Fill in the URL of your public Mendeley profile to import the references of your publications', 300);" onmouseout="tooltip.hide();"><sup>3</sup></a></p>
+					<em class="url">http://www.mendeley.com/profiles/</em>
 					<input id="mendeley_profile_input" name="profileId" type="text" size="20" value="cameron-neylon"/>
-					<button id="mendeley_profile">Add IDs!</button>
-					</p>
+					<button id="mendeley_profile">Import</button>
 					<div id="mendeley_profile_div">
 					</div>
-					<hr />
-			
-					<p>Mendeley public group publications:
-					<p><em>http://www.mendeley.com/group/</em>
+					</fieldset>
+
+					<fieldset><legend>Import from a Mendeley group</legend>			
+					<p>Your Mendeley group URL<a class="tooltip" onmouseover="tooltip.show('Fill in the URL of your public Mendeley to import the references shared within group</em>', 300);" onmouseout="tooltip.hide();"><sup>4</sup></a></p>
+					<em class="url">http://www.mendeley.com/group/</em>
 				    <input id="mendeley_group_input" name="groupId" type="text" size="20" value="1389803"/>
-					<button id="mendeley_group">Add IDs!</button>
-					</p>
+					<button id="mendeley_group">Import</button>
 					<div id="mendeley_group_div">
 					</div>
-					<hr />
+					</fieldset>
+
+					<fieldset id="quick_report_section" name="quick"><legend>Quick reports</legend>
+					<p>Retrieve IDs from your Mendeley contacts and public groups<a class="tooltip" onmouseover="tooltip.show('Fill in the URL of your public Mendeley profile to retrieve publications for your contacts or your public groups', 300);" onmouseout="tooltip.hide();"><sup>5</sup></a></p>
+			        <em class="url">http://www.mendeley.com/profiles/</em>
+		            <input id="quick_report_input" name="profileId" type="text" size="20" value="cameron-neylon"/>
+					<button id="quick_report">Import</button>
+					<div id="quick_report_div">
+					</div>
+					</fieldset>
+				</div>
 			
-					<p>Slideshare public slidedecks:
-					<p><em>http://www.slideshare.net/</em>
+				<div id="col3">
+					<fieldset><legend>Import from Slideshare</legend>			
+					<p>Your Slideshare profile URL<a class="tooltip" onmouseover="tooltip.show('Fill in your Slideshare profile to import your public slidedecks', 300);" onmouseout="tooltip.hide();"><sup>6</sup></a></p>
+					<em class="url">http://www.slideshare.net/</em>
 				    <input id="slideshare_profile_input" name="slideshareName" type="text" size="20" value="cavlec"/>
-					<button id="slideshare_profile">Add IDs!</button>
-					</p>
+					<button id="slideshare_profile">Import</button>
 					<div id="slideshare_profile_div">
 					</div>
-					<hr />
-			        
-					<p>Dryad data packages <br>(dc:contributor.author value in "Show Full Metadata"):
-					<p><em>Dryad author name</em>
+					</fieldset>
+
+					<fieldset><legend>Import from Dryad</legend>						        
+					<p>Your Dryad author name<a class="tooltip" onmouseover="tooltip.show('Fill in the dc:contributor.author value in <em>Show Full Metadata</em> to retrieve your datasets', 300);" onmouseout="tooltip.hide();"><sup>7</sup></a></p>
 				    <input id="dryad_profile_input" name="dryadName" type="text" size="20" value="Otto, Sarah P."/>
-					<button id="dryad_profile">Add IDs!</button>
+					<button id="dryad_profile">Import</button>
 					</p>
 					<div id="dryad_profile_div">
 					</div>
-					<hr />
+					</fieldset>
 
-					<p>PubMed IDs through Grant Numbers<br>
-					<p><em>Grant number</em>
+					<fieldset><legend>Import from PubMed</legend>
+					<p>Your Grant number<a class="tooltip" onmouseover="tooltip.show('Fill in your Grant number to retrieve publications from PubMed', 300);" onmouseout="tooltip.hide();"><sup>8</sup></a></p>
 				    <input id="pubmed_grant_input" name="grantId" type="text" size="20" value="U54-CA121852"/>
-					<button id="pubmed_grant">Add IDs!</button>
+					<button id="pubmed_grant">Import</button>
 					</p>
 					<div id="pubmed_grant_div">
 					</div>
-					<hr />
-						
+					</fieldset>
 				</div>
+
 			</div>
-			
-			<div id="quick_report_section">
-			<h2><a name="quick">quick reports</a></h2>
-				
-				<p>Want to see a quick report for your Mendeley contacts or public groups?</p>
-		        <p><em>http://www.mendeley.com/profiles/</em>
-	            <input id="quick_report_input" name="profileId" type="text" size="20" value="cameron-neylon"/>
-				<button id="quick_report">Pull Mendeley links</button>
-				</p>
-				<div id="quick_report_div">
-				</div>
-			</div>
-						
 			<!-- END input -->
 
 			<!-- START footer -->
 
-			<div id="twitterfeed">
+			<div id="twitterfeed" style="clear:both; padding-top: 30px">
 
 				<h2><a name="recent">recently-shared reports</a></h2>
 				<p>Check them out:</p>
@@ -214,11 +215,11 @@ $(document).ready(function(){
 					  height: 200,
 					  theme: {
 					    shell: {
-					      background: '#ccc',
+					      background: '#DDD',
 					      color: '#000'
 					    },
 					    tweets: {
-					      background: '#ccc',
+					      background: '#EEE',
 					      color: '#000',
 					      links: '#933'
 					    }
@@ -239,12 +240,12 @@ $(document).ready(function(){
 			</div>
 
 			<div id="footer">
-				<p>Missing something? See <a href="./about.php#Limitations">current limitations.</a></p>
-				<p>Reactions and bugs welcome to <a href="http://twitter.com/#!/totalimpactdev">@totalimpactdev</a></p>
-				
-				<p><strong>Total-Impact</strong> <a href="http://www.mendeley.com/blog/developer-resources/what-the-scientific-community-wants-computers-to-do-for-them-the-results-of-the-plos-and-mendeley-call-for-apps/">needs more developers!</a>  Join us? <a href="mailto:total-impact@googlegroups.com">total-impact@googlegroups.com</a></p>
-
-            	<p>Concept originally hacked at the <a href="http://www.beyond-impact.org/">Beyond Impact Workshop</a>. <a href="https://github.com/mhahnel/total-impact">Source and contributors.</a></p>
+			<h2>about</h2>
+            	<p><strong>Total-Impact</strong>. Concept originally hacked at the <a href="http://www.beyond-impact.org/">Beyond Impact Workshop</a> (<a href="https://github.com/mhahnel/total-impact">source and contributors</a>)</p>
+	            <p>All Total-impact data is available under a <a href="http://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a> license</p>
+				<p>Total-Impact <a href="http://www.mendeley.com/blog/developer-resources/what-the-scientific-community-wants-computers-to-do-for-them-the-results-of-the-plos-and-mendeley-call-for-apps/">needs more developers!</a>  Join us? <a href="mailto:total-impact@googlegroups.com">total-impact@googlegroups.com</a></p>
+				<p>Missing something? See a list of <a href="./about.php#Limitations">current limitations.</a> Reactions and bugs welcome to <a href="http://twitter.com/#!/totalimpactdev">@totalimpactdev</a></p>
+				<p><a class="img" href="http://altmetrics.org" title="an altmetrics project"><img src="./ui/img/altmetrics_logo.png" alt="altmetrics" width="80" style="margin-bottom:5px" /></a></p>
 			</div>
 			<!-- END footer -->
 		</div>
