bff1debe37d2e1b683ebafb4d5aab2e5779ed4e2
Heather Piwowar <hpiwowar@gmail.com>
2011-11-07 10:51:27 -0800

    tweaks to make run on server

--- api/v1/index.php
+++ api/v1/index.php
@@ -1,9 +1,16 @@
 <?php
 
 require_once '../../library/restler/restler.php';
+require_once 'items.php';
+require_once 'collections.php';
+require_once 'providers.php';
+require_once 'users.php';
+require_once 'v1HtmlFormat.php';
+require_once 'v1XmlFormat.php';
 
-spl_autoload_register('spl_autoload');
+#spl_autoload_register('spl_autoload');
 
+# give Restler an arg of TRUE in production to cache the url->method mappings
 $r = new Restler();
 $r->setSupportedFormats('JsonFormat', 'V1XmlFormat', 'V1HtmlFormat');
 $r->addAPIClass('Collections');
