05716fb58d38ffcf32c8d15f95a2f5fc2debd2ba
Jason Priem <jp@jasonpriem.org>
2011-11-19 02:08:33 -0500

    much messing about with htaccess...redirect now workds.

--- header.php
+++ header.php
@@ -39,7 +39,7 @@
     <body>
         <div id="header">
             <div class="wrapper">
-                <h1><span class="loathsome-hack"></span><a href="./index.php">total-impact</a><span class="loathsome-hack"></span></h1>
+                <h1><span class="loathsome-hack"></span><a href=".">total-impact</a><span class="loathsome-hack"></span></h1>
                 <ul id="nav">
                     <li><a href="./about.php">about</a></li>
                     <li><a href="http://twitter.com/#!/totalImpactdev">twitter</a></li>
